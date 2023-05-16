bef20dd627754dd76395a5922599641ba11923d4
Tony <greybluesea@gmail.com>
2023-05-16 20:33:12 +1200

    useInfiniteQuery replaced useQuery

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Button, HStack, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useFetchGames from "../hooks/useFetchGames";
 import GameCard from "./GameCard";
 import SkeletonCard from "./SkeletonCard";
@@ -10,29 +10,60 @@ interface Props {
 }
 
 const GameGrid = ({ queryGame }: Props) => {
-  const { resIncludingGames, error, isLoading } = useFetchGames(queryGame);
+  const {
+    resIncludingGames,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useFetchGames(queryGame);
   const skeletonArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   if (error) return <Text>{error.message}</Text>;
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, xl: 3, "2xl": 4 }}
-      spacing={5}
-      padding={2}
-    >
-      {isLoading
-        ? skeletonArray.map((each) => (
+    <>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, xl: 3, "2xl": 4 }}
+        spacing={5}
+        padding={2}
+      >
+        {isLoading
+          ? skeletonArray.map((each) => (
+              <GameCardContainer key={each}>
+                {" "}
+                <SkeletonCard /* key={each} */ />
+              </GameCardContainer>
+            ))
+          : resIncludingGames?.pages.map((page) =>
+              page.results.map((each) => (
+                <GameCardContainer key={each.id}>
+                  {" "}
+                  <GameCard /* key={each.id} */ game={each}></GameCard>
+                </GameCardContainer>
+              ))
+            )}
+        {isFetchingNextPage &&
+          skeletonArray.map((each) => (
             <GameCardContainer key={each}>
               {" "}
               <SkeletonCard /* key={each} */ />
             </GameCardContainer>
-          ))
-        : resIncludingGames?.results.map((each) => (
-            <GameCardContainer key={each.id}>
-              {" "}
-              <GameCard /* key={each.id} */ game={each}></GameCard>
-            </GameCardContainer>
           ))}
-    </SimpleGrid>
+      </SimpleGrid>
+      {hasNextPage && (
+        <HStack
+          marginTop={"3"}
+          marginBottom={"5"}
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
+            {/*  {isFetchingNextPage ? <Spinner /> : "Load More"} */} Load More
+          </Button>
+        </HStack>
+      )}
+    </>
   );
 };
 
--- src/hooks/useFetchGames.ts
+++ src/hooks/useFetchGames.ts
@@ -1,10 +1,38 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useInfiniteQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import { QueryGame } from "../App";
 import axiosClientForGames, { Game } from "../services/axiosClientForGames";
 import { ResFromFetch } from "../services/AXIOSClient";
 
 const useFetchGames = (queryGame: QueryGame) => {
+  const {
+    data: resIncludingGames,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  } = useInfiniteQuery<ResFromFetch<Game>, AxiosError>({
+    queryKey: ["/games", queryGame],
+    queryFn: ({ pageParam = 1 }) =>
+      axiosClientForGames.get({
+        params: {
+          genres: queryGame?.genre?.id,
+          parent_platforms: queryGame?.platform?.id,
+          ordering: queryGame?.sort?.value,
+          search: queryGame?.search,
+          page: pageParam,
+          page_size: 12,
+        },
+      }),
+    keepPreviousData: true,
+    getNextPageParam: (lastPage, allPages) => {
+      /* return lastPage.results.length > 0 ? allPages.length + 1 : undefined; */
+      return lastPage.next ? allPages.length + 1 : undefined;
+    },
+  });
+
+  /* const useFetchGames = (queryGame: QueryGame) => {
   const {
     data: resIncludingGames,
     error,
@@ -20,7 +48,7 @@ const useFetchGames = (queryGame: QueryGame) => {
           search: queryGame?.search,
         },
       }),
-  });
+  }); */
 
   /* const useFetchGames = (queryGame: QueryGame) => {
   const {
@@ -40,7 +68,14 @@ const useFetchGames = (queryGame: QueryGame) => {
     [queryGame]
   ); */
 
-  return { resIncludingGames, error, isLoading };
+  return {
+    resIncludingGames,
+    error,
+    isLoading,
+    fetchNextPage,
+    isFetchingNextPage,
+    hasNextPage,
+  };
 };
 
 export default useFetchGames;
