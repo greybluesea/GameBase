e03f6a73874ee5bf004e65dac0c10707ca8baea0
Tony <greybluesea@gmail.com>
2023-05-20 22:37:48 +1200

    put on ErrorGamePage for trial

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -2,13 +2,14 @@ import { Heading } from "@chakra-ui/react";
 import { useLocation } from "react-router-dom";
 import RetractableText from "../components/RetractableText";
 import useFetchGameDetails from "../hooks/useFetchGameDetails";
+import ErrorGamePage from "./ErrorGamePage";
 
 const GameDetailPage = () => {
   const { state } = useLocation();
   /* if (!state.id) return <ErrorGamePage />; */
   const { gameDetails, error, isLoading } = useFetchGameDetails(state.id);
 
-  if (error || !gameDetails) return <Heading>{error?.message}</Heading>;
+  if (error || !gameDetails) return <ErrorGamePage />;
 
   return (
     <>
